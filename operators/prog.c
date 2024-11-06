cc6a8a18311d9e3724dce7f28d4b9a828a90ac65
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2024-11-06 09:01:00 +0530

    Create prog.c

---
+++ operators/prog.c
@@ -0,0 +1,56 @@
+ /*
+ operators:
+ unary,binary - operators:
+ unary - ++,--,+(unaryplus),-(unaryminus) for signed integers
+ binary:
+ Arithmetic Operations:+,-,*,/(Returns the quotient quotient = dividend / divisor; )
+ ,%(Returns the remainder after diving the left operand with the right operand.)
+ 
+ Relational Operators:==,!=,<(lessthan),>(greaterthan),<=,>=
+ 
+ Logical Operators:
+ &&(Logical AND Returns true if both the operands are true.)
+ ||(Logical OR	Returns true if both or any of the operand is true.)
+ !(Logical NOT	Returns true if the operand is false.)
+ 
+ Bitwise Operators:
+&
+Bitwise AND	Performs bit-by-bit AND operation and returns the result.	a & b
+
+|
+Bitwise OR	Performs bit-by-bit OR operation and returns the result.	a | b
+
+^
+Bitwise XOR	Performs bit-by-bit XOR operation and returns the result.	a ^ b
+
+~
+Bitwise First Complement	Flips all the set and unset bits on the number.	~a
+
+<<
+Bitwise Leftshift	Shifts the number in binary form by one place in the operation and returns the result.	a << b
+
+>>
+Bitwise Rightshilft	Shifts the number in binary form by one place in the operation and returns the result.	a >> b
+ */
+ 
+#include<stdio.h>
+int main(){
+    //unary 
+    int a=10;
+    printf("unary\n");
+    printf("unary ++=%d\n",a++);
+    printf("unary ++=%d\n",++a);
+    printf("unary --=%d\n",a--);
+    printf("unary +=%d\n",+a);
+    printf("unary -=%d\n",-a);
+    
+    //binary 
+    a=10;
+    int b=20;
+    printf("binary\n");
+    printf("+=%d\n",a+b);
+    printf("-=%d\n",a-b);
+    printf("*=%d\n",a*b);
+    printf("/=%d\n",a/b);
+    return 0;
+}
