5f0d7b827bfbb8520ba52baf86c353dc3c2834e5
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2024-11-07 08:47:04 +0530

    Update prog.c

--- operators/prog.c
+++ operators/prog.c
@@ -65,5 +65,36 @@ int main(){
     printf(">%d\n",a>b);
     printf("<=%d\n",a<=b);
     printf(">=%d\n",a>=b);
+    
+    //Logical
+    a=10;
+    b=20;
+    int c=10;
+    printf("Logical\n");
+    printf("&&=%d\n",a==b&&b==c);
+    printf("||=%d\n",a==b||a==c);
+    printf("!=%d\n",!a==c);
+    
+    //Bitwise
+    a=10;
+    b=5;
+    printf("Bitwise\n");
+    printf("&=%d\n",a&b); 
+    /*
+        1 2 4 8
+        0 1 0 1 =10
+    &   1 0 1 0 =5
+        0 0 0 0
+    */
+    a=10;
+    b=5;
+    printf("|=%d\n",a|b);
+    /*
+      1 2 4 8
+      0 1 0 1 =10
+    | 1 0 1 0 =5
+      1 1 1 1 =15
+    */
     return 0;
 }
+
